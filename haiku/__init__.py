d0a0f388afe194135c9da40938cbc4ce486c42b0
Mark Friedenbach <mark@friedenbach.org>
2012-03-08 10:28:06 -0800

    Moved get_version() to setup.py, and removed space between release candidate/beta/alpha and number.

--- haiku/__init__.py
+++ haiku/__init__.py
@@ -33,21 +33,6 @@
 # DOCUMENTATION, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # ===----------------------------------------------------------------------===
 
-VERSION = (0,0,0, 'alpha', 0)
-
-def get_version():
-  version = '%s.%s' % (VERSION[0], VERSION[1])
-  if VERSION[2]:
-    version = '%s.%s' % (version, VERSION[2])
-  if VERSION[3:] == ('alpha', 0):
-    version = '%s pre-alpha' % version
-  else:
-    if VERSION[3] != 'final':
-      version = "%s %s" % (version, VERSION[3])
-      if VERSION[4] != 0:
-        version = '%s %s' % (version, VERSION[4])
-  return version
-
 # ===----------------------------------------------------------------------===
 # End of File
 # ===----------------------------------------------------------------------===
